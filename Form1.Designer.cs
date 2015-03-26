e623363b145e23e338c07f617d16bfce223e3a54
David Bauer <david.bauer009@gmail.com>
2015-03-26 18:56:27 +0100

    Upload to GitHub

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace MandelBrot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 715);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(619, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(12, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1750, 1000);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(657, 714);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numericUpDown1.Location = new System.Drawing.Point(738, 717);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            125,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(1158, 714);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 742);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "MandelBrot";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MandelBrot
+{
+    public partial class Form1 : Form
+    {
+        Bitmap map;
+        //const int WIDTH = 1750;
+        //const int HEIGHT = 1000;
+        const int WIDTH = 350;
+        const int HEIGHT = 200;
+        const double RATIO = (double) HEIGHT / WIDTH;
+        const double xRATIO = 2.5 / 3.5;
+
+        double currMini = 0, currMaxi = 0, currMinr = 0, currMaxr = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            map = new Bitmap(WIDTH, HEIGHT);
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawMandel(1,-2.5,1,-1);
+        }
+
+        private void DrawMandel(double maxr, double minr, double maxi, double mini)
+        {
+            currMaxi = maxi;
+            currMaxr = maxr;
+            currMini = mini;
+            currMinr = minr;
+
+            progressBar1.Value = 0;
+            int color = 0;
+            double cx = 0, cy = 0;
+            double zx = 0, zy = 0;
+            double iteration = 0, max_iteration = (int)numericUpDown1.Value;
+
+            double xjump = ((maxr - minr) / WIDTH);
+            double yjump = ((maxi - mini) / HEIGHT);
+
+            double tempzx = 0;
+
+            for (int x = 0; x < WIDTH; x++)
+            {
+                cx = (xjump * x) - Math.Abs(minr);
+                for (int y = 0; y < HEIGHT; y++)
+                {
+
+                    zx = 0;
+                    zy = 0;
+                    cy = (yjump * y) - Math.Abs(mini);
+                    iteration = 0;
+
+                    while (zx * zx + zy * zy <= 4 && iteration < max_iteration)
+                    {
+                        iteration++;
+                        tempzx = zx;
+                        zx = (zx * zx) - (zy * zy) + cx;
+                        zy = (2 * tempzx * zy) + cy;
+                    }
+
+                    color = (iteration < max_iteration) ? Convert.ToInt32((iteration / max_iteration) * 254) : 255;
+                    //color = (iteration < max_iteration) ? 0 : 255;
+                    map.SetPixel(x, y, Color.FromArgb(color%128*2, color&32*7, color%16*14));
+                    //map.SetPixel(x, y, Color.FromArgb(color, color, color));
+                }
+
+                progressBar1.Value = (int)Math.Ceiling(((double)x / WIDTH * 100));
+
+                //if (x % 20 == 0)
+                //    panel1.CreateGraphics().DrawImage(map, 0, 0);
+            }
+
+            panel1.CreateGraphics().DrawImage(map, 0, 0);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int ex = e.X;
+            int ey = e.Y;
+            double currentxjump = (currMaxr - currMinr) / WIDTH;
+            double currentyjump = (currMaxi - currMini) / HEIGHT;
+
+            int zoomx = WIDTH / 5;
+            int zoomy = HEIGHT / 5;
+
+            DrawMandel(((ex + zoomx) * currentxjump) - Math.Abs(currMinr), 
+                ((ex - zoomx) * currentxjump) - Math.Abs(currMinr), 
+                ((ey + zoomy) * currentyjump) - Math.Abs(currMini), 
+                ((ey - zoomy) * currentyjump) - Math.Abs(currMini));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DrawMandel(1, -2.5, 1, -1);
+        }
+
+    }
+}
